03b0bb560b03240c205e61b178ce6c7fc2ac3f6f
Mihir Ranade <techwizard01@hotmail.com>
2025-04-25 09:11:15 +0530

    Discord SSO :: cors option modified to allow frontend domain to store cookies

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  credentials: true,
+}));
 app.use(morgan("tiny"));
 app.disable("x-powered-by");
 const port = process.env.PORT || 8080;
